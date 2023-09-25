0e4a4ab1ac175f574033cdcc905c62c7255ed84d
booboosui <suixiaobo@kuafuai.net>
2023-09-25 10:44:37 +0800

    init

--- main.py
+++ main.py
@@ -4,18 +4,3 @@ from src.game import Game
 
 # Initialize the game
 pygame.init()
-
-# Set up the game window
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Flappy Bird")
-
-# Create a new game instance
-game = Game(screen)
-
-# Start the game
-game.run()
-
-# Quit the game
-pygame.quit()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,3 @@
 # Utility functions for the game
 import pygame
 
-def load_image(image_path):
-    image = pygame.image.load(image_path)
-    return image.convert_alpha()
