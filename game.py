5ea76388f013cf6e4188f3edfa14cf28d6528e02
charging-kuafuai <charging@126.com>
2024-01-31 16:23:30 +0800

    一个菱形网格地图 地图上有各种地形，山地，丛林，河流，沙漠，河畔 山地 在山地方的，伤害增加1点，防御增加1点，饥渴状态提高1 在丛林的一方，伤害降低1点，防御提高1点 在河流上，伤害增加2点，防御降低2点 在沙漠上，伤害降低3点，防御增加3点 在河畔上，伤害增加1点，防御降低1点
    玩法
    自主移动，每个兵每个回合只能移动一次和攻击一次。
    有步兵，炮兵，坦克，骑兵，护卫舰，轰炸机
    把敌方全部领地占领完即可获得胜利
    可以联机对战，也可以人机对战
    
    步兵生命值10，对地攻击2，对空1，对海1，每次只能移动1-3格，以自身为中心
    炮兵生命值8，对攻击4，对空2，对海3，每次只能移动一格，以自己为中心
    坦克生命值20，对地攻击8，对空2，对海攻击4，每次移动1-3格，以自己为中心
    骑兵生命值6，对地攻击3，对空1，对海1，每次移动1-9格，但是只能对角线走
    护卫舰生命值20，对地攻击4，对空3，对海8，只能在海上走，每次移动0-9格，自身为中心
    轰炸机生命值10，对地攻击8，对空1，对海8，全图行走，每次移动0-5格
    
    起始金币100
    用金币可以建造兵房100金币，在兵房用金币可以造兵
    每一块土地每一个回合可以获得5金币
    步兵50金币
    炮兵250金币
    坦克500金币
    骑兵100金币
    护卫舰1000金币
    轰炸机600金币

--- game.py
+++ game.py
@@ -1,31 +1,133 @@
 import pygame
-from pygame.locals import *
+import random
+
+# 游戏地图的宽度和高度
+MAP_WIDTH = 800
+MAP_HEIGHT = 600
+
+# 游戏兵种的速度
+UNIT_SPEED = 2
+
+# 游戏建筑的建造时间
+BUILDING_BUILD_TIME = 5
+
+# 游戏玩家的初始金币数量
+PLAYER_INITIAL_GOLD = 100
+
+# 游戏玩家的初始兵种列表
+PLAYER_INITIAL_UNITS = ["Infantry", "Artillery"]
+
+# 游戏玩家的初始建筑列表
+PLAYER_INITIAL_BUILDINGS = ["Barracks"]
+
+# 游戏兵种的属性
+UNIT_PROPERTIES = {
+    "Infantry": {
+        "health": 100,
+        "attack": 10,
+        "defense": 5,
+        "range": 1,
+        "speed": 1,
+        "cost": 10
+    },
+    "Artillery": {
+        "health": 50,
+        "attack": 20,
+        "defense": 2,
+        "range": 3,
+        "speed": 1,
+        "cost": 20
+    },
+    "Tank": {
+        "health": 200,
+        "attack": 30,
+        "defense": 10,
+        "range": 1,
+        "speed": 2,
+        "cost": 30
+    },
+    "Cavalry": {
+        "health": 80,
+        "attack": 15,
+        "defense": 3,
+        "range": 1,
+        "speed": 3,
+        "cost": 15
+    },
+    "Warship": {
+        "health": 150,
+        "attack": 25,
+        "defense": 8,
+        "range": 2,
+        "speed": 2,
+        "cost": 25
+    },
+    "Bomber": {
+        "health": 100,
+        "attack": 50,
+        "defense": 1,
+        "range": 4,
+        "speed": 4,
+        "cost": 50
+    }
+}
+
+# 游戏建筑的属性
+BUILDING_PROPERTIES = {
+    "Barracks": {
+        "health": 100,
+        "build_time": 5,
+        "cost": 50
+    }
+}
+
+# 游戏玩家的属性
+PLAYER_PROPERTIES = {
+    "Player 1": {
+        "gold": 100,
+        "units": ["Infantry", "Artillery"],
+        "buildings": ["Barracks"]
+    },
+    "Player 2": {
+        "gold": 100,
+        "units": ["Infantry", "Artillery"],
+        "buildings": ["Barracks"]
+    }
+}
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((MAP_WIDTH, MAP_HEIGHT))
         self.clock = pygame.time.Clock()
         self.is_running = True
 
     def handle_events(self):
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 self.is_running = False
 
     def update(self):
         pass
 
     def draw(self):
-        self.screen.fill((255, 255, 255))
-        pygame.display.flip()
-
-    def game_over(self):
         pass
+
+    def start(self):
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            pygame.display.flip()
+            self.clock.tick(60)
 
 class WorldConquest(Game):
     def __init__(self):
         super().__init__()
+        self.create_map()
+        self.create_units()
+        self.create_buildings()
+        self.create_players()
 
     def create_map(self):
         pass
@@ -79,12 +181,4 @@ class WorldConquest(Game):
         pass
 
     def start(self):
-        while self.is_running:
-            self.handle_events()
-            self.update()
-            self.draw()
-            self.clock.tick(60)
-
-if __name__ == "__main__":
-    game = WorldConquest()
-    game.start()
+        super().start()
