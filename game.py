3ae05bf4349aac04d42424ac75fc1a6910e8a50f
charging-kuafuai <charging@126.com>
2024-01-31 19:29:44 +0800

    一个菱形网格地图 地图上有各种地形，山地，丛林，河流，沙漠，河畔 山地 在山地一方的，伤害增加1点，防御增加1点 在丛林的一方，伤害降低1点，防御提高1点 在河流上，伤害增加2点，防御降低2点 在沙漠上，伤害降低3点，防御增加3点 在河畔上，伤害增加1点，防御降低1点
    玩法
    自主移动，每个兵每个回合只能移动一次和攻击一次。
    有步兵，炮兵，坦克，骑兵，护卫舰，轰炸机
    把敌方全部领地占领完即可获得胜利
    可以联机对战，也可以人机对战
    
    步兵生命值10，对地攻击2，对空1，对海1，每次只能移动1-3格，以自身为中心
    炮兵生命值8，对攻击4，对空2，对海3，每次只能移动一格，以自己为中心
    坦克生命值20，对地攻击8，对空2，对海攻击4，每次移动1-3格，以自己为中心
    骑兵生命值6，对地攻击3，对空1，对海1，每次移动1-9格，但是只能对角线走
    护卫舰生命值20，对地攻击4，对空3，对海8，只能在海上走，每次移动0-9格，自身为中心
    轰炸机生命值10，对地攻击8，对空1，对海8，全图行走，每次移动0-5格
    
    起始金币100
    用金币可以建造兵房100金币，在兵房用金币可以造兵
    每一块土地每一个回合可以获得5金币
    步兵50金币
    炮兵250金币
    坦克500金币
    骑兵100金币
    护卫舰1000金币
    轰炸机600金币

--- game.py
+++ game.py
@@ -1,1 +1,261 @@
-在`game.py`文件中创建一个名为`WorldConquest`的类，继承自`Game`类。
+import pygame
+import random
+
+# Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Define screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Define unit types
+UNIT_TYPES = ["Infantry", "Cavalry", "Artillery"]
+
+# Define terrain types
+TERRAIN_TYPES = ["Grassland", "Forest", "Mountain"]
+
+# Define unit stats
+UNIT_STATS = {
+    "Infantry": {"health": 100, "attack": 10, "defense": 5, "movement": 2},
+    "Cavalry": {"health": 150, "attack": 15, "defense": 10, "movement": 3},
+    "Artillery": {"health": 75, "attack": 20, "defense": 2, "movement": 1},
+}
+
+# Define barracks stats
+BARRACKS_STATS = {"cost": 100, "unit_costs": {"Infantry": 50, "Cavalry": 75, "Artillery": 100}}
+
+# Define player stats
+PLAYER_STATS = {"gold": 1000}
+
+# Define AI stats
+AI_STATS = {"gold": 1000}
+
+class WorldConquest(Game):
+    def __init__(self):
+        super().__init__()
+        self.map = Map()
+        self.player = Player("Player")
+        self.ai = AI()
+        self.barracks = Barracks()
+        self.units = []
+        self.current_unit = None
+        self.current_action = None
+        self.current_target = None
+        self.game_over = False
+
+    def start_game(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("World Conquest")
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.display_menu()
+
+    def display_menu(self):
+        # TODO: Display game menu
+        pass
+
+    def display_rules(self):
+        # TODO: Display game rules
+        pass
+
+    def display_map(self):
+        self.map.display_map()
+
+    def display_units(self):
+        # TODO: Display unit information
+        pass
+
+    def display_barracks(self):
+        # TODO: Display barracks information
+        pass
+
+    def display_player(self):
+        # TODO: Display player information
+        pass
+
+    def display_ai(self):
+        # TODO: Display AI information
+        pass
+
+    def display_victory(self):
+        # TODO: Display victory message
+        pass
+
+    def display_defeat(self):
+        # TODO: Display defeat message
+        pass
+
+    def get_input(self):
+        # TODO: Get user input
+        pass
+
+    def deploy_units(self):
+        # TODO: Deploy units on the map
+        pass
+
+    def attack(self):
+        # TODO: Perform attack action
+        pass
+
+    def move(self):
+        # TODO: Perform move action
+        pass
+
+    def build_barracks(self):
+        # TODO: Build barracks on the map
+        pass
+
+    def produce_units(self):
+        # TODO: Produce units in the barracks
+        pass
+
+    def check_victory(self):
+        # TODO: Check victory condition
+        pass
+
+    def end_game(self):
+        self.running = False
+
+class Map:
+    def __init__(self):
+        self.terrain = [[random.choice(TERRAIN_TYPES) for _ in range(10)] for _ in range(10)]
+
+    def display_map(self):
+        for row in self.terrain:
+            for terrain in row:
+                print(terrain, end=" ")
+            print()
+
+    def update_map(self):
+        # TODO: Update map after actions
+        pass
+
+    def get_terrain(self, x, y):
+        return self.terrain[y][x]
+
+    def set_terrain(self, x, y, terrain):
+        self.terrain[y][x] = terrain
+
+class Unit:
+    def __init__(self, unit_type):
+        self.unit_type = unit_type
+        self.health = UNIT_STATS[unit_type]["health"]
+        self.attack = UNIT_STATS[unit_type]["attack"]
+        self.defense = UNIT_STATS[unit_type]["defense"]
+        self.movement = UNIT_STATS[unit_type]["movement"]
+
+    def get_unit_type(self):
+        return self.unit_type
+
+    def get_health(self):
+        return self.health
+
+    def get_attack(self):
+        return self.attack
+
+    def get_defense(self):
+        return self.defense
+
+    def get_movement(self):
+        return self.movement
+
+    def set_health(self, health):
+        self.health = health
+
+    def set_attack(self, attack):
+        self.attack = attack
+
+    def set_defense(self, defense):
+        self.defense = defense
+
+    def set_movement(self, movement):
+        self.movement = movement
+
+class Barracks:
+    def __init__(self):
+        self.cost = BARRACKS_STATS["cost"]
+        self.unit_costs = BARRACKS_STATS["unit_costs"]
+
+    def get_cost(self):
+        return self.cost
+
+    def get_unit_cost(self, unit_type):
+        return self.unit_costs[unit_type]
+
+    def build(self):
+        # TODO: Build barracks on the map
+        pass
+
+    def produce(self, unit_type):
+        # TODO: Produce units in the barracks
+        pass
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.gold = PLAYER_STATS["gold"]
+
+    def get_name(self):
+        return self.name
+
+    def get_gold(self):
+        return self.gold
+
+    def set_gold(self, gold):
+        self.gold = gold
+
+    def add_gold(self, gold):
+        self.gold += gold
+
+    def subtract_gold(self, gold):
+        self.gold -= gold
+
+class AI:
+    def __init__(self):
+        self.gold = AI_STATS["gold"]
+
+    def deploy_units(self):
+        # TODO: Deploy units on the map
+        pass
+
+    def attack(self):
+        # TODO: Perform attack action
+        pass
+
+    def move(self):
+        # TODO: Perform move action
+        pass
+
+    def build_barracks(self):
+        # TODO: Build barracks on the map
+        pass
+
+    def produce_units(self):
+        # TODO: Produce units in the barracks
+        pass
+
+class GameController:
+    def __init__(self):
+        self.game = WorldConquest()
+
+    def start_game(self):
+        self.game.start_game()
+
+    def play_turn(self):
+        # TODO: Play a turn of the game
+        pass
+
+    def end_game(self):
+        self.game.end_game()
+
+def main():
+    controller = GameController()
+    controller.start_game()
+
+if __name__ == "__main__":
+    main()
