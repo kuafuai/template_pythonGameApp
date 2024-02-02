5be400f14b6e9e13aeb05282998177b0e8fd14b6
charging-kuafuai <charging@126.com>
2024-02-02 22:17:48 +0800

    1.用adb连接手机，打开抖音app 2.判断男女可选 3.判断关注量可选范围，比如我想要700以内就输入700 4.判断粉丝量可选范围，比如我想要2000以内就输入2000 5.点开评论可选三条随机评论，评论表可以自定义txt文件 6.判断年龄可输入范围，比如我只想要25-45岁以内的

--- game.py
+++ game.py
@@ -1,8 +1,71 @@
-# Import necessary modules
-import pygame
+import random
+import subprocess
 
-# Implement all specific logic of the game
 class Game:
     def __init__(self):
-        pygame.init()
+        self.adb = Adb()
+
+    def select_gender(self, gender):
+        # TODO: Implement gender selection logic
+        pass
+
+    def select_followers(self, max_followers):
+        # TODO: Implement followers selection logic
+        pass
+
+    def select_fans(self, max_fans):
+        # TODO: Implement fans selection logic
+        pass
+
+    def select_age(self, min_age, max_age):
+        # TODO: Implement age selection logic
+        pass
+
+    def select_comments(self, comments_file):
+        # TODO: Implement comments selection logic
+        pass
+
+    def like_and_comment(self):
+        # TODO: Implement like and comment logic
+        pass
+
+    def custom_comment(self, comment):
+        # TODO: Implement custom comment logic
+        pass
+
+    def open_douyin(self):
+        self.adb.connect_phone()
+        self.adb.open_app("com.ss.android.ugc.aweme")
+        
+
+class Comment:
+    def __init__(self, content):
+        self.content = content
+
+    def get_content(self):
+        return self.content
+
+
+class CommentTable:
+    def __init__(self, comments_file):
+        self.comments = self.load_comments(comments_file)
+
+    def load_comments(self, comments_file):
+        # TODO: Implement loading comments from file logic
+        pass
+
+    def get_comments(self):
+        return self.comments
+
+
+class Adb:
+    def __init__(self):
+        pass
+
+    def connect_phone(self):
+        # TODO: Implement connecting to phone logic
+        pass
+
+    def open_app(self, app_name):
+        # TODO: Implement opening app logic
         pass
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-# Import necessary modules
 import pygame
-from game import Game
 
-# Initialize the game
+class DouyinGame:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Douyin Game")
+
+    def main(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.fill((255, 255, 255))
+            pygame.display.flip()
+
+        pygame.quit()
+
 def main():
-    pygame.init()
-    game = Game()
-
-if __name__ == "__main__":
-    main()
+    game = DouyinGame()
+    game.main()
