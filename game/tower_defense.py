c7398d67c299d62ff31496bf74df8e172cd84c97
charging-kuafuai <charging@126.com>
2024-06-01 17:32:09 +0800

    自我修复

---
+++ game/tower_defense.py
@@ -0,0 +1,98 @@
+import pygame
+
+class TowerDefense:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def build_tower(self, position):
+        pass
+
+    def upgrade_tower(self, tower):
+        pass
+
+    def sell_tower(self, tower):
+        pass
+
+    def spawn_enemy(self):
+        pass
+
+    def check_collision(self, sprite1, sprite2):
+        pass
+
+    def game_over(self):
+        pass
+
+
+class Tower:
+    def __init__(self, position):
+        pass
+
+    def upgrade(self):
+        pass
+
+    def sell(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Enemy:
+    def __init__(self, path):
+        pass
+
+    def move(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Bullet:
+    def __init__(self, position, target):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Map:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Button:
+    def __init__(self, position, text):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Text:
+    def __init__(self, position, text):
+        pass
+
+    def draw(self):
+        pass
---
+++ main/main.py
@@ -0,0 +1,8 @@
+from game import TowerDefense
+
+def main():
+    game = TowerDefense()
+    game.run()
+
+if __name__ == "__main__":
+    main()
